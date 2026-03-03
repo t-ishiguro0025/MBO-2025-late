a3b5087f24476a52a142364f7464b45d7a9da91e
t.ishiguro <t.ishiguro@sonicmoov.com>
2026-03-03 10:04:43 +0900

    fix: 空文字用のスキーマを定義

--- app/features/auth/schemas/loginSchema.ts
+++ app/features/auth/schemas/loginSchema.ts
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
+const emailOrEmptySchema = z.string().email().or(z.literal(""));
+const passwordOrEmptySchema = z.string().min(8).or(z.literal(""));
+
 export const loginSchema = z.object({
   email: z
     .string()
     .min(1, { message: "メールアドレスを入力してください" })
-    .refine((v) => z.string().email().or(z.literal("")).safeParse(v).success, {
+    .refine((v) => emailOrEmptySchema.safeParse(v).success, {
       message: "正しいメールアドレスを入力してください",
     }),
   password: z
     .string()
     .min(1, { message: "パスワードを入力してください" })
-    .refine((v) => z.string().min(8).or(z.literal("")).safeParse(v).success, {
+    .refine((v) => passwordOrEmptySchema.safeParse(v).success, {
       message: "パスワードは8文字以上で入力してください",
     }),
 });
