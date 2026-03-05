e9e21717c32bac798fa4075cf3a9e646c4a1601b
t.ishiguro <t.ishiguro@sonicmoov.com>
2026-03-05 09:37:27 +0900

    fix: LoginページにContainerをおく

--- app/routes/_auth/login.tsx
+++ app/routes/_auth/login.tsx
@@ -1,14 +1,5 @@
-import { LoginForm } from "@/features/auth";
+import { LoginFormContainer } from "@/features/auth";
 
 export default function LoginPage() {
-  return (
-    <LoginForm
-      formValue={{ email: "", password: "" }}
-      isLoading={false}
-      apiError={undefined}
-      fieldErrors={{}}
-      onEmailChange={() => {}}
-      onPasswordChange={() => {}}
-    />
-  );
+  return <LoginFormContainer />;
 }
