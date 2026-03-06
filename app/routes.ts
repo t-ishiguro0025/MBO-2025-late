5595c4173996dea2c7806c6c12627a1296efcc7e
SMV/t-ishiguro <t.ishiguro@sonicmoov.com>
2026-03-06 23:22:35 +0900

    Add new route for logs in app/routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,7 @@ export default [
   layout("routes/_auth/layout.tsx", [
     route("/login", "routes/_auth/login.tsx"),
   ]),
+  layout("routes/_app/layout.tsx", [
+    route("/logs", "routes/_app/logs.tsx"),
+  ]),
 ] satisfies RouteConfig;
