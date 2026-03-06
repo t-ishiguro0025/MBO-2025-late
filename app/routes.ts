39042620991d47211e286a608e6a8a7ad904e20c
t.ishiguro <t.ishiguro@sonicmoov.com>
2026-03-06 22:51:45 +0900

    fix: add root route

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
   layout("routes/_auth/layout.tsx", [
-    route("/", "routes/_auth/login.tsx"), // 追加
+    route("/", "routes/_auth/login.tsx"),
     route("/login", "routes/_auth/login.tsx"),
   ]),
 ] satisfies RouteConfig;
